2b16df29145c51f58feef2fddc516a1eaaf06042
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2021-12-05 10:32:39 +0700

    Add files via upload

---
+++ lajas.js
@@ -0,0 +1,7 @@
+var app = new Vue({ 
+    el: '#app',
+});
+document.write( '<div class="col-12 col-md-12 p-3 p-md-5"><h1>Get Started With LavaBlogs</h1><p>Play video and get started using lava blog for build modern your web app, or you can visit documentation details here <a href="https://www.hockeycomputindo.com/2021/02/new-laravel-blog-cms-free-download.html">documentation →</a>.</p><p>Hey we have develope a new source code cms blog using laravel and sqlite you most download for free here.. <a href="https://www.hockeycomputindo.com/2021/12/laravel-website-cms-source-code-full.html" class="btn btn-danger">Download new laravel CMS →</a></p><div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/7ZJ2NEszW-0" allowfullscreen></iframe></div></div>' );
+
+
+
